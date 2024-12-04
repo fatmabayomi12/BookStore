d9f563b4815cb11420201f2c6c3948ad89cc54c6
Fatma Bayomi <your-email@example.com>
2024-12-04 19:32:25 +0200

    fatma

---
+++ data.js
@@ -0,0 +1,74 @@
+const books = [
+  {
+    title: "How to Stop Time",
+    author: "",
+    description: "About How to Stop Time",
+    price: 45,
+  },
+  {
+    title: "Written in Bone",
+    author: "",
+    description: "About Written in Bone",
+    price: 35,
+  },
+  {
+    title: "The Comfort Boo",
+    author: "",
+    description: "About The Comfort Boo",
+    price: 55,
+  },
+  {
+    title: "The Safest Lies",
+    author: "",
+    description: "About The Safest Lies",
+    price: 40,
+  },
+  {
+    title: "One Of Us Is Nexte",
+    author: "",
+    description: "About One Of Us Is Nexte",
+    price: 65,
+  },
+  {
+    title: "The Giver of Stars",
+    author: "",
+    description: "About The Giver of Stars",
+    price: 45,
+  },
+  {
+    title: "Losing Hope",
+    author: "",
+    description: "About The Giver of Stars",
+    price: 30,
+  },
+];
+
+const authors = [
+  {
+    firstName: "Robert",
+    lastName: "Kiyosaki",
+    nationality: "USA",
+  },
+  {
+    firstName: "Paulo",
+    lastName: "Coelho",
+    nationality: "Brazil",
+  },
+  {
+    firstName: "Wayne",
+    lastName: "Dyer",
+    nationality: "USA",
+  },
+  {
+    firstName: "Rolf",
+    lastName: "Dobelli",
+    nationality: "Switzerland",
+  },
+];
+
+
+
+module.exports = {
+  books,
+  authors
+};
